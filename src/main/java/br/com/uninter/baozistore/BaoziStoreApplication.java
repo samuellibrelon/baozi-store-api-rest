af755b06356c7ad582afe5385a29371efa0fb58d
Samuel Librelon <127879852+samuellibrelon@users.noreply.github.com>
2026-08-23 17:33:20 -0300

    Adiciona classe principal Spring Boot

---
+++ src/main/java/br/com/uninter/baozistore/BaoziStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.uninter.baozistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoziStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoziStoreApplication.class, args);
+	}
+
+}
